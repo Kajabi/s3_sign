101f67b2c533c99f61171fe1f130fdfaf38ea369
Rob Christie <rob.christie@kajabi.com>
2024-12-31 15:47:03 -0700

    add region for specs

--- spec/s3_sign_spec.rb
+++ spec/s3_sign_spec.rb
@@ -4,8 +4,7 @@ require "spec_helper"
 
 describe S3Sign do
   before do
-    Aws.config.update(credentials: Aws::Credentials.new("spec_access_key_id", "spec_secret_access_key"))
-
+    Aws.config.update(credentials: Aws::Credentials.new("spec_access_key_id", "spec_secret_access_key"), region: "us-east-1")
     described_class.bucket_name = "spec_bucket"
   end
 
